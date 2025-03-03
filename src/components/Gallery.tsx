51d8eba41e62b32938b8444717d3ba7990dfe017
Ivanelektricar <ivanellec4@idvinced.com>
2025-03-03 20:25:56 +0100

    Update Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,7 +5,7 @@ const Gallery: React.FC = () => {
   const photos = [
     {
       id: 1,
-      src: "components/476233885_10235391858165550_8115568287395250994_n.jpg",
+      src: "src/components/image/1.jpg",
       caption: " \"Пумпај протест, не пумпај оцене!\"",
       location: "Испред топоницу"
     },
